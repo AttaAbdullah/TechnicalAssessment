5809bed40e951714d8af2fce961eb6b969f10702
AttaAbdullah <atta.abdullah@hotmail.com>
2021-02-04 22:35:22 -0500

    first commit

---
+++ NumbersToWords/NumbersToWords/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NumbersToWords
+{
+    class Program
+    {
+        static void NumbersToWords(int number)
+        {
+            String[] units = { "Zero", "One", "Two", "Three",                           // Initializing word map
+            "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+            "Seventeen", "Eighteen", "Nineteen" };
+
+            String[] tens = {"", "", "Twenty", "Thirty", "Forty",
+             "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+            while (number < 0 || number > 999)                                          // Checking if input is within range 
+            {
+                Console.WriteLine("Error: Number out of range.\nEnter a number between 0 and 999.");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (number < 20)                                                            // Checking value of number to return approriate word elements 
+                Console.WriteLine(units[number]);
+
+            else if (number < 100 && number >= 20)
+            {
+                if (number % 10 == 0)
+                    Console.WriteLine(tens[number / 10]);
+
+                else
+                    Console.WriteLine($"{tens[number / 10]} {units[number % 10]}");
+            }
+
+            else if (number > 99)
+            {
+                if (number % 100 == 0)
+                    Console.WriteLine($"{units[number / 100]} hundred");
+
+                else if (number % 100 != 0 && number % 10 == 0)
+                    Console.WriteLine($"{units[number / 100]} hundred {tens[(number % 100) / 10].ToLower()}");
+
+                else if (number % 100 != 0 && number % 10 != 0 && number % 100 < 20)
+                    Console.WriteLine($"{units[number / 100]} hundred and {units[number % 100].ToLower()}");
+
+                else
+                    Console.WriteLine($"{units[number / 100]} hundred {tens[(number % 100) / 10].ToLower()} {units[number % 10].ToLower()}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int number = 183;
+
+            NumbersToWords(number);
+        }
+    }
+}
---
+++ UpnGenerator/UpnGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UpnGenerator
+{
+    class Program
+    {
+        public static string UPNGenerator(string companyCode, int productCode)
+        {
+            productCode += 1;                                                           // Increasing productCode by 1
+
+            string tempCode = string.Format("{0}{1}", companyCode, productCode);        // Initializing temporary code to calculate check digit
+
+            int evenSum = 0;                                                            // Initializing variables
+            int oddSum = 0;
+            int digit;
+            int checkDigit;
+
+            for (int i = 1; i < tempCode.Length + 1; i++)                               // Iterating over temporary code to extract even and odd digit sums
+            {
+                digit = Convert.ToInt32(tempCode.Substring(i - 1, 1));                  // Extracting current digit 
+
+                if (i % 2 == 0)
+                    evenSum += digit;
+                else
+                    oddSum += digit;
+            }
+
+            checkDigit = (((oddSum * 3) + evenSum) % 10 == 0) ? 0 : (10 - (((oddSum * 3) + evenSum) % 10)); // Calculating checkDigit
+
+            string upnCode = string.Format("{0}{1}{2}", companyCode, productCode, checkDigit);
+
+            return upnCode;
+        }
+
+        static void Main(string[] args)
+        {
+            const string companyCode = "036000";
+
+            int productCode = 24144;
+
+            Console.WriteLine(UPNGenerator(companyCode, productCode));
+        }
+    }
+}
